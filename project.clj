c7cedd788778f8054771be38f5f328d9d19510fe
Konrad Tallman <konradt95@gmail.com>
2018-01-15 13:44:46 -0600

    Adds unit tests and updates project.clj and CHANGELOG.md

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clj-ftpserver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clj-ftpserver "0.1.0"
+  :description "A wrapper around the Apache FtpServer library"
+  :url "https://github.com/komcrad/clj-ftpserver"
+  :license {:name "GNU Lesser Public License"
+            :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.ftpserver/ftpserver-core "1.1.1"]])
+                 [org.apache.ftpserver/ftpserver-core "1.1.1"]]
+  :profiles {:dev {:dependencies [[com.velisco/clj-ftp "0.3.9"]
+                                  [komcrad-utils "0.1.2"]]}})
--- test/clj_ftpserver/core_test.clj
+++ test/clj_ftpserver/core_test.clj
@@ -1,7 +1,50 @@
 (ns clj-ftpserver.core-test
   (:require [clojure.test :refer :all]
-            [clj-ftpserver.core :refer :all]))
+            [clj-ftpserver.core :refer :all]
+            [miner.ftp :as ftp]
+            [komcrad-utils.io :as io]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest users-test
+  (testing "users"
+    (let [test-users (users [{:username "bob" :password "password1" :home-dir "/tmp/bob/"}
+                             {:username "billy" :password "password2" :home-dir "/tmp/billy" :write true}])]
+      (let [user (first test-users)]
+        (is "bob" (.getName user))
+        (is "password1" (.getPassword user))
+        (is "/tmp/bob/" (.getHomeDirectory user)))
+      (let [user (second test-users)]
+        (is "billy" (.getName user))
+        (is "password2" (.getPassword user))
+        (is "/tmp/billy" (.getHomeDirectory user))
+        (is (= org.apache.ftpserver.usermanager.impl.WritePermission
+               (type (first (.getAuthorities user)))))))))
+
+(deftest ftp-server-test
+  (testing "ftp-server")
+  (let [home-dir (io/tmp-folder)
+        server (ftp-server [{:username "bob" :password "password" :home-dir (.getPath home-dir)}] {:port 5555})]
+    (io/with-tmp-file home-dir
+      (spit (str (.getPath home-dir) "/foo.txt") "I'm foo.txt")
+      (with-ftp-server server
+        (ftp/with-ftp [client "ftp://bob:password@127.0.0.1:5555/"]
+          (ftp/client-get client "foo.txt" (str (.getPath home-dir) "/foo2.txt"))
+          (is (= "I'm foo.txt" (slurp (str (.getPath home-dir) "/foo2.txt"))))))))
+  (let [home-dir (io/tmp-folder)
+        server (ftp-server [{:username "bob" :password "password" :home-dir (.getPath home-dir)}]
+                           {:port 5555 :ssl true})]
+    (io/with-tmp-file home-dir
+      (spit (str (.getPath home-dir) "/foo.txt") "I'm foo.txt")
+      (with-ftp-server server
+        (ftp/with-ftp [client "ftps://bob:password@127.0.0.1:5555/"]
+          (ftp/client-get client "foo.txt" (str (.getPath home-dir) "/foo2.txt"))
+          (is (= "I'm foo.txt" (slurp (str (.getPath home-dir) "/foo2.txt"))))))))
+
+  (let [home-dir (io/tmp-folder)
+        server (ftp-server [{:username "bob" :password "password" :home-dir (.getPath home-dir)}]
+                           {:port 5555})]
+    (io/with-tmp-file home-dir
+      (spit (str (.getPath home-dir) "/foo.txt") "I'm foo.txt")
+      (with-ftp-server server
+        (is (thrown? javax.net.ssl.SSLException
+                     (ftp/with-ftp [client "ftps://bob:password@127.0.0.1:5555/"]
+                       (ftp/client-get client "foo.txt" (str (.getPath home-dir) "/foo2.txt")))))))))
