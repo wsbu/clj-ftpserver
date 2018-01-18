3546a5b94a82710b5ee696e7aa8638e47d83f2a0
komcrad <konradt95@gmail.com>
2018-01-18 12:03:55 -0600

    Fixes keystore issue when being used as a library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
+## [Unreleased]
+### Fixed
+- issue with the keystore not being copied out of the library jar properly
+
 ## [0.1.0] - 2018-01-15
 ### Added
 - wrapper around apache ftpserver
--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-ftpserver "0.1.0"
+(defproject clj-ftpserver "0.1.1-SNAPSHOT"
   :description "A wrapper around the Apache FtpServer library"
   :url "https://github.com/komcrad/clj-ftpserver"
   :license {:name "GNU Lesser Public License"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.ftpserver/ftpserver-core "1.1.1"]]
-  :profiles {:dev {:dependencies [[com.velisco/clj-ftp "0.3.9"]
-                                  [komcrad-utils "0.1.2"]]}})
+                 [org.apache.ftpserver/ftpserver-core "1.1.1"]
+                 [komcrad-utils "0.1.5-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[com.velisco/clj-ftp "0.3.9"]]}})
