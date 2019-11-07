4ab1a36636e03fd7106a57ac27ad5d449f20db80
SgtZdog <kevin@zemon.name>
2019-11-07 12:58:42 -0600

    [MAN-1720]

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.ftpserver/ftpserver-core "1.1.1"]
                  [komcrad-utils "0.1.5"]]
-  :profiles {:dev {:dependencies [[com.velisco/clj-ftp "0.3.9"]]}})
+  :profiles {:dev {:dependencies [[com.velisco/clj-ftp "0.3.9"]]}}
+  :deploy-repositories [["releases" {:url "https://mvn.redlion.net/repository/maven-releases"
+                                     :sign-releases false}]
+                        ["snapshots" {:url "https://mvn.redlion.net/repository/maven-snapshots"
+                                      :sign-releases false}]])
